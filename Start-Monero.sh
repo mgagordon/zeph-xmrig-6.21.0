5afd4beba7170d033886624b49368498279cda79
mgagordon <160228671+mgagordon@users.noreply.github.com>
2024-02-17 16:16:29 +0300

    Add files via upload

---
+++ Start-Monero.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./xmrig --coin XMR --url "xmr.kryptex.network:7777" --user 48edfHu7V9Z84YzzMa6fUueoELZ9ZRXq9VetWzYGzKt52XU5xvqgzYnDK9URnRoJMk1j8nLwEVsaSWJ4fhdUyZijBGUicoD/MyFirstRig -p x -k
---
+++ Start-Zephyr.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+./xmrig --coin zephyr --url "zeph.kryptex.network:7777" --user ZEPHYR2HUt7AjqqTzNG8CPjbMG14hDCCHWcggX4BNfG3NaQFpWyWdMSYgibwUGQBMqGFZaBScjxtgS8YrXwVWMsKeW6y1UzPYqr4B/ilya111 -p x -k
